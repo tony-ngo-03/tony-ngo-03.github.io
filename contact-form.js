af9cd125d40be70777a1fad696fb82b1c0df44f2
tony-ngo-03 <tonyngo@utexas.edu>
2024-09-29 13:31:01 -0500

    fixed contact form

--- contact-form.js
+++ contact-form.js
@@ -2,9 +2,9 @@ const form = document.getElementById("contact-form");
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
+    const name = document.getElementById('name-input').value;
+    const email = document.getElementById('email-input').value;
+    const message = document.getElementById('message-input').value;
 
     const subject = encodeURIComponent("New message from " + name);
     const body = encodeURIComponent(`Name: ${name}\nEmail: ${email}\n\nMessage:\n${message}`);
